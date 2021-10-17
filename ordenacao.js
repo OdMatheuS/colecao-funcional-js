a861a9eebb047b4aade30f0762537a87bc4932e0
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-16 21:57:15 -0300

    ordenacao por idade utilizando sort() ramda

---
+++ ordenacao.js
@@ -0,0 +1,29 @@
+const R = require('ramda')
+
+const person = [{
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+},{   
+  firstName: "Lee",
+  lastName: "Janson",
+  age: 20,
+  eyeColor: "brown"
+},{   
+  firstName: "Shadow",
+  lastName: "Foo",
+  age: 40,
+  eyeColor: "green"
+}, {   
+  firstName: "Will",
+  lastName: "Match",
+  age: 21,
+  eyeColor: "none"
+}];
+
+const ordAsc = R.ascend(R.prop('age'))
+
+const peopleOrdByAgeAsc = R.sort(ordAsc,person)
+
+console.log(peopleOrdByAgeAsc)
