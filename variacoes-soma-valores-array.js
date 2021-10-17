56277d7ae952f0038ccaef10d3687829db533805
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-16 22:27:41 -0300

    comparando soma de valores em uma colecao com ramda vs reduce tradicional

---
+++ variacoes-soma-valores-array.js
@@ -0,0 +1,14 @@
+const R = require('ramda')
+
+const randomCollection = [12, 10, 9, 1, 7, 3, 5, 12, 5, 6, 1, 9, 12, 2, 2];
+
+//tradicional / mdn
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+const resultReduce = randomCollection.reduce(reducer)
+console.log(`resultado reduce tradicional:${resultReduce}`);
+
+
+const somaValoresArrayRamda = R.sum(randomCollection);
+
+console.log(`resultado total soma valores com ramda:${somaValoresArrayRamda}`);
